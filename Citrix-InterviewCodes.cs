1101bea3768ad12ce2642e07d9c47a0731379236
Arjun K Prasad <arjun.blr1237@gmail.com>
2018-10-31 00:35:40 +0000

    Update Citrix-InterviewCodes.cs

--- Citrix-InterviewCodes.cs
+++ Citrix-InterviewCodes.cs
@@ -27,22 +27,7 @@ class Solution
         var valuePairs = extractInfo(counts);
         var keyList = valuePairs.Keys.ToList();
 
-        HashSet<string> distictV = new HashSet<string>();
-
-        foreach (KeyValuePair<string, int> Pair in valuePairs)
-        {
-            string word = Pair.Key;
-            distictV.Add(word);
-            while (word.Contains("."))
-            {
-
-                var index = word.IndexOf(".");
-                //var len = word.Length - index - 1;
-                word = word.Substring(index+1);
-                if (word != "")
-                    distictV.Add(word);
-            }
-        }
+        HashSet<string> distictV = getDistinct(valuePairs);
 
         Dictionary<string, int> hits = new Dictionary<string, int>();
 
@@ -56,34 +41,43 @@ class Solution
                     hits[val] += Pair.Value;
                 }
             }
-        }
-        
+        }       
 
         foreach(var hit in hits)
         {
             Console.WriteLine(hit.Value +"      "+ hit.Key);
         }
+    }
 
+    static HashSet<string> getDistinct(Dictionary<string, int> valuePairs)
+    {
+        HashSet<string> distictV = new HashSet<string>();
+        foreach (KeyValuePair<string, int> Pair in valuePairs)
+        {
+            string word = Pair.Key;
+            distictV.Add(word);
+            while (word.Contains("."))
+            {
+                var index = word.IndexOf(".");
+                word = word.Substring(index + 1);
+                if (word != "")
+                    distictV.Add(word);
+            }
+        }
+        return distictV;
     }
 
 
     static Dictionary<string, int> extractInfo(string[] counts)
     {
         var Pairs = new Dictionary<string, int>();
-
         for (int i = 0; i < counts.Length; i++)
         {
-
-
             var temp = counts[i];
             string[] words = temp.Split(',');
-            //Console.WriteLine(words[0]);
             int val = Int32.Parse(words[0]);
-
             Pairs.Add(words[1], val);
         }
-
         return Pairs;
-
     }
 }
